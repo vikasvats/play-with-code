20a1d6950ac0d61effb1681c23ac5036cec7b4f2
vikas chaudhary <124273440+vikasvats@users.noreply.github.com>
2024-12-25 11:58:53 +0530

    Find Maximum of two numbers in Python

---
+++ python_Learn_2025/Q2_gfg.py
@@ -0,0 +1,20 @@
+# https://www.geeksforgeeks.org/maximum-of-two-numbers-in-python/?ref=lbp
+
+# find the miximum of two numbers
+
+a = 9
+b = 3
+
+# print(max(a,b))
+
+# using conditional statements
+
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+# print using ternary operator
+
+res = a if a >b else b 
+print(res)
